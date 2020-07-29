7a572f9a9d0b35dcd4d369cadcbedbcb9ea2c5fb
xavier-ux <xcollins90@gmail.com>
2020-07-29 15:56:27 -0400

    1st update

--- MusicRater.Data/Album.cs
+++ MusicRater.Data/Album.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,18 @@ namespace MusicRater.Data
 {
     public class Album
     {
+        [Key]
+        public int AlbumId { get; set; }
+        [Required]
+        public Guid OwnerId { get; set; }
+        [Required]
+        public string AlbumName { get; set; }
+        [Required]
+        public decimal Rating { get; set; }
+        public virtual Artist Artist { get; set; }//Should be foreign key
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        //[DefaultValue(false)] not sure if we need this 
+        //public bool IsRated { get; set; }
     }
 }
---
+++ MusicRater.Models/ListAlbums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicRater.Models
+{
+    public class ListAlbums
+    {
+        public int AlbumId { get; set; }
+        public string AlbumName { get; set; }
+        public decimal Rating { get; set; }
+        public DateTimeOffset CreatedUtc { get; set; }
+    }
+}

de0424431d1ac3f56e562c0ec5dcba8d1d05e7ac
JeffRichardson1150 <jeffrichardson573@gmail.com>
2020-07-30 23:03:22 -0400

    CRUD for Artist

--- MusicRater/Controllers/ArtistController.cs
+++ MusicRater/Controllers/ArtistController.cs
@@ -2,10 +2,7 @@
 using MusicRater.Models;
 using MusicRater.Services;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
 namespace MusicRater.Controllers
@@ -19,6 +16,13 @@ namespace MusicRater.Controllers
             var artists = artistService.GetArtists();
             return Ok(artists);
         } // Get
+
+        public IHttpActionResult Get(int id)
+        {
+            ArtistService artistService = CreateArtistService();
+            var artists = artistService.GetArtistById(id);
+            return Ok(artists);
+        } // Get by ID
 
         public IHttpActionResult Post(ArtistCreate artist)
         {
@@ -40,5 +44,26 @@ namespace MusicRater.Controllers
             return artistService;
         } // CreateArtistService
 
+        public IHttpActionResult Put(ArtistEdit artist)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateArtistService();
+
+            if (!service.UpdateArtist(artist))
+                return InternalServerError();
+
+            return Ok();
+        } // Put
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateArtistService();
+
+            if (!service.DeleteArtist(id))
+                return InternalServerError();
+
+            return Ok();
+        } // Delete
     }
 }
